af0e49dc1e8ca5a9aab84d6800b288f210d9f432
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 10:49:02 +0100

    feat: implement 005.slice solution

--- quests/005.slice/solution.go
+++ quests/005.slice/solution.go
@@ -1,7 +1,27 @@
 package slice
 
 func ProcessScores(scores []int) []int {
-	//TODO: implement
-	// Read README.md for the instructions
-	return []int{}
+	processed := []int{}
+	for _, score := range scores {
+		if score < 0 || score > 100 {
+			continue
+		} else if score < 40 {
+			score = 40
+		}
+
+		processed = append(processed, score)
+	}
+
+	bonusPoints := len(processed) > 5
+	for i, score := range processed {
+		if bonusPoints {
+			score += 5
+		}
+		if score > 100 {
+			score = 100
+		}
+
+		processed[i] = score
+	}
+	return processed
 }
