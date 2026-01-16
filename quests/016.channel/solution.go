3ddb33448060055c050b597356274f9b51e1747f
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 15:49:02 +0100

    feat: implement 016.channel

--- quests/016.channel/solution.go
+++ quests/016.channel/solution.go
@@ -1,19 +1,23 @@
 package channel
 
-// TODO:Implement ReadFromBoth function
-// Read README.md for the instructions
+import "fmt"
+
 func ReadFromBoth(ch1 chan string, ch2 chan string) string {
-	return ""
+	val1 := <-ch1
+	val2 := <-ch2
+
+	return fmt.Sprintf("read: %s & %s", val1, val2)
 }
 
-// TODO:Implement ReadFromBoth function
-// Read README.md for the instructions
 func WriteToBoth(ch1 chan string, ch2 chan string, msg string) {
-
+	go func() {
+		formatted := "write: " + msg
+		ch1 <- formatted
+		ch2 <- formatted
+	}()
 }
 
-// TODO:Implement ReadFromBoth function
-// Read README.md for the instructions
 func ReadThenWrite(input chan string, output chan string) {
-
+	val := <-input
+	output <- fmt.Sprintf("transform: %s", val)
 }
