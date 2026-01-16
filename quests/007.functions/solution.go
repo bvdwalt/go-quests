ffff0c3cd807943001a790dd08e8e38a8d157905
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 11:36:33 +0100

    feat: implement 007.functions

--- quests/007.functions/solution.go
+++ quests/007.functions/solution.go
@@ -1,30 +1,56 @@
 package functions
 
+import (
+	"errors"
+	"strings"
+)
+
 // Divide returns a / b or an error if b == 0
 func Divide(a, b int) (int, error) {
-	// TODO: implement
-	// Read README.md for instructions on returning proper errors
-	return 0, nil
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
 }
 
 // SumAll returns the sum of all provided integers
 func SumAll(nums ...int) int {
-	// TODO: implement
-	// Read README.md for instructions on handling variadic arguments
-	return 0
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
 }
 
 // MaxMin returns the max and min of all provided integers
 // Returns an error if no numbers are provided
 func MaxMin(nums ...int) (int, int, error) {
-	// TODO: implement
-	// Read README.md for instructions on proper error handling
-	return 0, 0, nil
+	if len(nums) == 0 {
+		return 0, 0, errors.New("no numbers provided")
+	}
+	max, min := nums[0], nums[0]
+	for _, n := range nums[1:] {
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
+	}
+	return max, min, nil
 }
 
 // ConcatAll joins all strings using the provided separator
 func ConcatAll(sep string, strs ...string) string {
-	// TODO: implement
-	// Read README.md for instructions on variadic strings
-	return ""
+	if len(strs) == 0 {
+		return ""
+	}
+	var builder strings.Builder
+	for i, s := range strs {
+		if i > 0 {
+			builder.WriteString(sep)
+		}
+		builder.WriteString(s)
+	}
+	return builder.String()
 }
