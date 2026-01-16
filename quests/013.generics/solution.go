b25c48c8c73b430511f945a955860dce8eab54fd
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 14:56:37 +0100

    feat: implement 013.generics

--- quests/013.generics/solution.go
+++ quests/013.generics/solution.go
@@ -11,18 +11,50 @@ type element[T any] struct {
 
 // Push adds a value to the end of the list.
 func (lst *List[T]) Push(v T) {
-	// TODO: implement
+	newElem := &element[T]{val: v}
+
+	if lst.head == nil {
+		lst.head = newElem
+		lst.tail = newElem
+		return
+	}
+	lst.tail.next = newElem
+	lst.tail = newElem
 }
 
 // Pop removes the last element and returns it.
 func (lst *List[T]) Pop() (T, bool) {
-	// TODO: implement
-	var zero T
-	return zero, false
+	var last = lst.tail
+
+	if last == nil {
+		var zero T
+		return zero, false
+	}
+
+	if lst.head == lst.tail {
+		lst.head = nil
+		lst.tail = nil
+		return last.val, true
+	}
+
+	current := lst.head
+	for current.next != lst.tail {
+		current = current.next
+	}
+
+	current.next = nil
+	lst.tail = current
+
+	return last.val, true
 }
 
 // AllElements returns a slice of all elements in the list.
 func (lst *List[T]) AllElements() []T {
-	// TODO: implement
-	return nil
+	result := []T{}
+
+	for current := lst.head; current != nil; current = current.next {
+		result = append(result, current.val)
+	}
+
+	return result
 }
