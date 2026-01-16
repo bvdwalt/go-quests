7afe265df8d30b7a032bd049e47af50e859c06af
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 16:16:33 +0100

    feat: implement 018.tickers

--- quests/018.tickers/solution.go
+++ quests/018.tickers/solution.go
@@ -1,7 +1,25 @@
 package tickers
 
-func Ticker() {
-	// TODO:Implement Ticker function
-	// Read README.md for the instructions
+import (
+	"fmt"
+	"time"
+)
 
+func Ticker() {
+	helloTicker := time.NewTicker(500 * time.Millisecond)
+	worldTicker := time.NewTicker(1 * time.Second)
+	timeout := time.After(3 * time.Second)
+	defer helloTicker.Stop()
+	defer worldTicker.Stop()
+
+	for {
+		select {
+		case <-helloTicker.C:
+			fmt.Println("hello")
+		case <-worldTicker.C:
+			fmt.Println("world")
+		case <-timeout:
+			return
+		}
+	}
 }
