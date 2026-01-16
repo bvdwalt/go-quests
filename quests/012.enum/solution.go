37807fee547ce28c814ab475838b40c89f5f1ae1
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-01-16 14:39:14 +0100

    feat: implement 012.enum

--- quests/012.enum/solution.go
+++ quests/012.enum/solution.go
@@ -1,5 +1,7 @@
 package enum
 
+import "fmt"
+
 type OrderState int
 
 const (
@@ -12,12 +14,67 @@ const (
 	StateReturned
 )
 
+type OrderAction string
+
+const (
+	ActionPay     OrderAction = "pay"
+	ActionPack    OrderAction = "pack"
+	ActionShip    OrderAction = "ship"
+	ActionDeliver OrderAction = "deliver"
+	ActionCancel  OrderAction = "cancel"
+	ActionReturn  OrderAction = "return"
+)
+
+var stateActionMappings = map[OrderState]string{
+	StateCreated:   "created",
+	StatePaid:      "paid",
+	StatePacked:    "packed",
+	StateShipped:   "shipped",
+	StateDelivered: "delivered",
+	StateCancelled: "cancelled",
+	StateReturned:  "returned",
+}
+
 func (s OrderState) String() string {
-	// TODO: implement
-	return ""
+	return stateActionMappings[s]
 }
 
 func NextState(current OrderState, action string) OrderState {
-	// TODO: implement
-	return current
+	switch current {
+	case StateCreated:
+		if action == string(ActionPay) {
+			return StatePaid
+		} else if action == string(ActionCancel) {
+			return StateCancelled
+		}
+		return current
+	case StatePaid:
+		if action == string(ActionPack) {
+			return StatePacked
+		} else if action == string(ActionCancel) {
+			return StateCancelled
+		}
+		return current
+	case StatePacked:
+		if action == string(ActionShip) {
+			return StateShipped
+		}
+		return current
+	case StateShipped:
+		if action == string(ActionDeliver) {
+			return StateDelivered
+		} else if action == string(ActionReturn) {
+			return StateReturned
+		}
+		return current
+	case StateDelivered:
+		if action == string(ActionReturn) {
+			return StateReturned
+		}
+		return current
+	case StateCancelled, StateReturned:
+		return current
+	default:
+		panic(fmt.Errorf("unknown state: %s", current))
+	}
 }
